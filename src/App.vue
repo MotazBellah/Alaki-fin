d74f615150bc3ff7bf377d8132e68a21a11b60e7
Moataz Bellah Ghobashy <engineer.android@yahoo.com>
2020-11-15 23:02:34 +0200

    Added custom popup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,7 @@
 <template>
   <div id="app">
-
-
-              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-              <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@500&display=swap" rel="stylesheet">
-              
-
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/cats">Cats</router-link> |
-      <router-link to="/dogs">Dogs</router-link> |
-      <router-link to="/pets">Pets</router-link>
-    </div>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@500&display=swap" rel="stylesheet">
     <router-view />
   </div>
 </template>
